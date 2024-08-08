c4fd261775ad5a300fc20026e065bdb3cfdb4d6d
Bine Cadez <bine.cadez@gmail.com>
2024-08-08 20:33:29 +0200

    added landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,16 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        cover: "url('images/cover.jpg')",
+      },
+      colors: {
+        baseGreen: "#D0E1D4",
+        brightOrange: "#E4BE9E",
+        slightGray: "#707070",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
