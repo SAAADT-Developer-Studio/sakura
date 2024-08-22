4dcc470523fe1add8e6ed1382e2db079be49ba8f
Bine Cadez <bine.cadez@gmail.com>
2024-08-22 19:32:51 +0200

    dynamic button addition

--- app/components/profile.tsx
+++ app/components/profile.tsx
@@ -3,23 +3,30 @@ import { Form } from "@remix-run/react";
 import logout from "../images/logout.png";
 import account_circle from "../images/account_circle.png";
 
-export default function Profile({ userName }: { userName: string }) {
+export default function Profile({
+  userName,
+  setLoggedInStyle,
+}: {
+  userName: string;
+  setLoggedInStyle: React.Dispatch<React.SetStateAction<string>>;
+}) {
   const [dropdownToggled, setDropdownToggled] = React.useState(false);
   return (
-    <div className="flex flex-col justify-center place-items-center">
+    <div className="flex flex-col justify-between place-items-center w-16 h-28 mt-2">
       <img
-        className="w-10 h-10"
+        className="w-10 h-10 cursor-pointer"
         onClick={() => setDropdownToggled(!dropdownToggled)}
         src={account_circle}
         alt="profile"
       />
       {dropdownToggled ? (
-        <div className="bg-brightOrange p-2 rounded-xl">
+        <div className="p-2 rounded-xl flex justify-center place-items-center flex-col">
           <h1 className="flex justify-center place-items-center text-lg">
             {userName}
           </h1>
           <Form method="POST" action="/logout" className="w-16">
             <button
+              onClick={() => setLoggedInStyle("justify-between")}
               type="submit"
               className="w-full flex justify-between place-items-center text-lg"
             >
